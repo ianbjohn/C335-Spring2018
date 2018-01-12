d89a82166880b7e06c3823798be530f5d972e207
Ian B Johnson <ianbjohn@shale.soic.indiana.edu>
2018-01-12 09:46:10 -0500

    Hello world prints every three seconds

---
+++ Lab1/hello.c
@@ -0,0 +1,16 @@
+//C335
+//Lab 1
+//Ian Johnson
+//1/12/18
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+  while (1) {
+    printf("Hello, world!\n");
+    sleep(3);
+  }
+
+  return 0;
+}
