f8324dc477cb522c50e838711070e079a499b7f1
Ian B Johnson <ianbjohn@marble.soic.indiana.edu>
2018-01-29 18:16:24 -0500

    Lab 3 should work now

---
+++ driver/inc/f3d_user_btn.h
@@ -0,0 +1,3 @@
+/* declarations for f3d_user_btn.c */
+void f3d_user_btn_init();
+int f3d_user_btn_read();
---
+++ lab3/main.c
@@ -0,0 +1,56 @@
+//C335
+//Lab 3
+//Authors:
+//-Ian Johnson (ianbjohn)
+//-Timothy Jones (jonestg)
+
+#include <stm32f30x.h>  // Pull in include files for F30x standard drivers 
+#include <f3d_led.h>     // Pull in include file for the local drivers
+#include <f3d_user_btn.h>
+
+// Simple looping delay function
+void delay(void) {
+  int i = 2000000;
+  while (i-- > 0) {
+    asm("nop"); /* This stops it optimising code out */
+  }
+}
+
+int main(void){
+  //set up the LEDs and buttons
+  f3d_user_btn_init();
+  f3d_led_init();
+  f3d_led_all_off();
+
+  int counter = 1; //keeps track of which LEDs should currently be lit
+  int pause = 0;  //toggles between 0 and 1 when the button is pressed. Program is paused when this is set to 1
+  while(1) {
+    if (f3d_user_btn_read())
+      pause ^= 1;
+
+    if (!pause) {
+      f3d_led_on(counter);
+      delay();
+      if (++counter >= 8) {
+	//flash if the counter rolled over 8, and then reset counter
+	f3d_led_all_off();
+	delay();
+	f3d_led_all_on();
+	delay();
+	f3d_led_all_off();
+	delay();
+	counter = 0;
+      }
+    }
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+/* Infinite loop */
+/* Use GDB to find out why we're here */
+  while (1);
+}
+#endif
+
+/* main.c ends here */
