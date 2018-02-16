64a33db5b06c3cc80b0b41c734058bca7ad26269
Ian B Johnson <ianbjohn@marble.soic.indiana.edu>
2018-02-16 09:41:24 -0500

    Fixed led code

--- driver/src/f3d_led.c
+++ driver/src/f3d_led.c
@@ -32,7 +32,7 @@ void f3d_led_on(int led) {
 /*Turns off the approiate led as specified by the parameter*/ 
 void f3d_led_off(int led) {
   GPIOE->BRR = GPIO_Pin_8 << (led + 1) % 8;
-} 
+}
 
 /*Turns on all LEDs*/
 void f3d_led_all_on(void) {
@@ -50,48 +50,4 @@ void f3d_led_all_off(void) {
   }
 } 
 
-/* led.c ends here */
-/*Turns off the approiate led as specified by the parameter*/ 
-void f3d_led_off(int led) {
-  switch(led){
-  case 0:
-    GPIOE->BRR = GPIO_Pin_8;
-    break;
-  case 1:
-    GPIOE->BRR = GPIO_Pin_9;
-    break;
-  case 2:
-    GPIOE->BRR = GPIO_Pin_10;
-    break;
-  case 3:
-    GPIOE->BRR = GPIO_Pin_11;
-    break;
-  case 4:
-    GPIOE->BRR = GPIO_Pin_12;
-    break;
-  case 5:
-    GPIOE->BRR = GPIO_Pin_13;
-    break;
-  case 6:
-    GPIOE->BRR = GPIO_Pin_14;
-    break;
-  case 7:
-    GPIOE->BRR = GPIO_Pin_15;
-    break;
-  default:
-    break;
-  }
-} 
-
-/*Turns on all LEDs*/
-void f3d_led_all_on(void) {
-  GPIOE->BSRR =GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
-} 
-
-/*Turns off all LEDs*/
-void f3d_led_all_off(void) {
-  GPIOE->BRR = GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
-} 
-
-/* led.c ends here */
 
