c9ddf53c506180de170b6c16c61c453033b5d46f
Ian B Johnson <ianbjohn@shale.soic.indiana.edu>
2018-02-28 13:05:58 -0500

    More progress on lab 7

--- lab7/main.c
+++ lab7/main.c
@@ -21,6 +21,9 @@
 #define TIMER 20000
 #define MAX 2
 
+void draw_dot(uint8_t, uint8_t);
+void erase_dot(uint8_t, uint8_t);
+
 int main(void) {
   // Set up your inits before you go ahead
    f3d_uart_init();
@@ -42,15 +45,15 @@ int main(void) {
 
    float accel_data[3], accel_data_old[3], mag_data[3], mag_data_old[3];
    int mode; //0 - tilt angle, 1 - compass
-   float pitch, roll;
+   float pitch, roll, xh, yh, heading;
 
    f3d_lcd_fillScreen2(RED);
 
    f3d_accel_read(accel_data);
    f3d_mag_read(mag_data);
 
-   printf("%f\n%f\n", accel_data[0], accel_data[1], accel_data[2]);
-   printf("%f\n%f\n", mag_data[0], mag_data[1], mag_data[2]);
+   //printf("%f\n%f\n", accel_data[0], accel_data[1], accel_data[2]);
+   //printf("%f\n%f\n", mag_data[0], mag_data[1], mag_data[2]);
 
    while(1) {
      //keep track of current tiltx and tilty, and the old tiltx and tilty
@@ -58,17 +61,17 @@ int main(void) {
      if (f3d_user_btn_read() == 1) {
        mode ^= 1; //toggle mode
        f3d_lcd_fillScreen2(RED); //eventually add graphical setup functions for the two mode, and call the right one
+       if (mode == 1) {
+	 f3d_lcd_drawChar(ST7735_width / 2, 0, 'N', BLACK, RED);
+	 f3d_lcd_drawChar(ST7735_width - 8, ST7735_height / 2, 'E', BLACK, RED);
+	 f3d_lcd_drawChar(ST7735_width / 2, ST7735_height - 8, 'S', BLACK, RED);
+	 f3d_lcd_drawChar(0, ST7735_height / 2, 'W', BLACK, RED);
+       }
        delay(10);
      } else if (mode == 0) {
-       //move current data to old data
-       int i;
-       for (i = 0; i < 3; i++)
-	 accel_data_old[i] = accel_data[i];
-
-       //clear old spot //maybe just try drawing 3x3 pixels
-       pitch = atan2f(accel_data_old[0], sqrt(pow(accel_data_old[1], 2) + pow(accel_data_old[2], 2)));
-       roll = atan2f(accel_data_old[1], sqrt(pow(accel_data_old[0], 2) + pow(accel_data_old[2], 2)));
-       f3d_lcd_fillArea((ST7735_width / 2) + (((int)(pitch * (float)(ST7735_width / 2))) / MAX) - 2, (ST7735_height / 2) + (((int)(roll * (float)(ST7735_height / 2))) / MAX) - 2, 4, 4, RED);
+       //TILT ANGLE
+       //clear old spot
+       erase_dot((ST7735_width / 2) - (((int)(roll * (float)(ST7735_width / 2))) / MAX) + 1, (ST7735_height / 2) - (((int)(pitch * (float)(ST7735_height / 2))) / MAX) + 1);
 
        //get new accel data
        f3d_accel_read(accel_data);
@@ -77,14 +80,47 @@ int main(void) {
        roll = atan2f(accel_data[1], sqrt(pow(accel_data[0], 2) + pow(accel_data[2], 2)));
 
        //draw new spot
-       f3d_lcd_fillArea((ST7735_width / 2) + (((int)(pitch * (float)(ST7735_width / 2))) / MAX) - 2, (ST7735_height / 2) + (((int)(roll * (float)(ST7735_width / 2))) / MAX) - 2, 4, 4, WHITE);
+       draw_dot((ST7735_width / 2) - (((int)(roll * (float)(ST7735_width / 2))) / MAX) + 1, (ST7735_height / 2) - (((int)(pitch * (float)(ST7735_height / 2))) / MAX) + 1);
      } else if (mode == 1) {
+       //COMPASS HEADING
+       //clear old spot
+       erase_dot((ST7735_width / 2) - (((int)(cos(heading) * (float)(ST7735_width / 2))) / MAX) + 1, (ST7735_height / 2) - (((int)(sin(heading) * (float)(ST7735_height / 2))) / MAX) + 1);
 
+       //get new accel and mag data
+       f3d_accel_read(accel_data);
+       f3d_mag_read(mag_data);
+
+       pitch = atan2f(accel_data[0], sqrt(pow(accel_data[1], 2) + pow(accel_data[2], 2)));
+       roll = atan2f(accel_data[1], sqrt(pow(accel_data[0], 2) + pow(accel_data[2], 2)));
+       xh = (mag_data[0] * cos(pitch)) + (mag_data[2] * sin(pitch));
+       yh = (mag_data[0] * sin(roll) * sin(pitch)) + (mag_data[1] * cos(roll)) - (mag_data[2] * sin(roll) * cos(pitch));
+       heading = atan2f(yh, xh);
+
+       //draw new spot
+       draw_dot((ST7735_width / 2) - (((int)(cos(heading) * (float)(ST7735_width / 2))) / MAX) + 1, (ST7735_height / 2) - (((int)(sin(heading) * (float)(ST7735_height / 2))) / MAX) + 1);
      }
      //printf("X: %f\nY: %f\nZ: %f\n\n", accel_data[0], accel_data[1], accel_data[2]);
      printf("Pitch: %f\nRoll: %f\n\n", pitch, roll);
      //delay(TIMER / 100);
    }
+}
+
+void draw_dot(uint8_t x, uint8_t y) {
+  //draws 9 white pixels to represent the dot
+  uint8_t i, j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      f3d_lcd_drawPixel(x + j, y + i, WHITE);
+  }
+}
+
+void erase_dot(uint8_t x, uint8_t y) {
+  //draws 9 red pixels to represent the dot being erased
+  uint8_t i, j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      f3d_lcd_drawPixel(x + j, y + i, RED);
+  }
 }
 
 #ifdef USE_FULL_ASSERT
