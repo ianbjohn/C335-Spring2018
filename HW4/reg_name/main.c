920bc450c6644115e9f78e723d3bff5c3d4cd311
Ian B Johnson <ianbjohn@shale.soic.indiana.edu>
2018-04-05 17:51:06 -0400

    Fixed register names in reg_name

--- HW4/reg_name/main.c
+++ HW4/reg_name/main.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include "reg_name.h"
 
-const char* names[16] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
+const char* names[16] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "sp", "lr", "pc"};
 
 int main()
 {
