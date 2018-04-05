ed2fcdcdf8d5e26724fd6ae748d95b1b03fc0af1
Ian B Johnson <ianbjohn@shale.soic.indiana.edu>
2018-04-05 16:59:48 -0400

    reg_name works

---
+++ HW4/reg_name/main.c
@@ -0,0 +1,19 @@
+//Ian Johnson
+//C335
+//HW4 - Register name test suite
+//4/5/18
+
+#include <stdio.h>
+#include "reg_name.h"
+
+const char* names[16] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
+
+int main()
+{
+  printf("String:\tString returned by function call:\n");
+  int i;
+  for (i = 0; i < 16; i++)
+    printf("%s\t%s\n", names[i], reg_name(i));
+
+  return 0;
+}
---
+++ HW4/reg_name/reg_name.h
@@ -0,0 +1,6 @@
+//Ian Johnson
+//C335
+//HW4 - register names
+//4/5/18
+
+char* reg_name(int i);
