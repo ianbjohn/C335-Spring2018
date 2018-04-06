a27849f87e5aa0df4cc8cebaef25215084631352
Ian B Johnson <ianbjohn@onyx.soic.indiana.edu>
2018-04-06 11:56:46 -0400

    Lab 11 progress

---
+++ lab11/queue.c
@@ -0,0 +1,79 @@
+/* queue.c --- 
+ * 
+ * Filename: queue.c
+ * Description: 
+ * Author: Bryce Himebaugh
+ * Maintainer: 
+ * Created: Thu Feb  7 19:49:26 2013 (-0500)
+ * Version: 
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * URL: 
+ * Doc URL: 
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* Code: */
+
+#include "queue.h"
+
+void init_queue(queue_t *buf) {
+  buf->head = 0;
+  buf->tail = 0;
+}
+
+int enqueue(queue_t *buf, int data) {
+  buf->tail++; //go to next position
+  buf->tail %= QUEUE_SIZE; //circle around
+  if (queue_empty(buf))  //checking to see if it's full in this case, actually
+    return 0;
+
+  buf->buffer[buf->tail] = data;
+  return 1; //success
+}
+
+int dequeue(queue_t *buf) {
+  if (queue_empty(buf))
+    return 0;
+
+  buf->head++;
+  buf->head %= QUEUE_SIZE;
+  return buf->buffer[buf->head];
+}
+
+int queue_empty(queue_t *buf) {
+  return (buf->head == buf->tail);
+}
+
+
+/* queue.c ends here */
